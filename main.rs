d74866c3397e4351c9a9f059b4b6f6d878298288
Landon Colaresi <landoncol3117@gmail.com>
2020-06-06 15:13:08 +0000

    begin

--- main.rs
+++ main.rs
@@ -4,23 +4,30 @@ fn main() {
 
 fn build_nd_transform(data: Vec<Vec<f64>>) -> Vec<f64> {
   data
-  .iter()
-  .map(|x| {get_nd(x,data)})
-  .collect::<Vec<f64>>()
+    .iter()
+    .map(|x| {get_nd(x,data)})
+    .collect::<Vec<f64>>()
 }
 
 fn get_nd(point: Vec<f64>, data: Vec<Vec<f64>>) -> f64 {
   let options: Vec<f64> = data
-  .iter()
-  .map(|x| {l2d_not_self(x,point)})
-  .collect::<Vec<f64>>();
-  f64min(options)
+    .iter()
+    .map(|x| {l2d_not_self(x,point)})
+    .collect::<Vec<f64>>();
+    f64min(options)
 }
 
 fn crossing(a: Vec<f64>, b: Vec<f64>, data: Vec<Vec<f64>>, ndt: Vec<f64>, eta: f64) -> bool {
   let regionpts: Vec<Vec<f64>> = data
     .iter()
     .filter(|&x| {in_region(a,b,eta,(*x).clone())})
+    .collect::<Vec<&Vec<f64>>>()
+    .iter()
+    .map(|&x| {*x})
+    .collect::<Vec<Vec<f64>>>();
+  let acceptpts: Vec<Vec<f64>> = regionpts
+    .iter()
+    .filter(|&x| {})
     .collect::<Vec<&Vec<f64>>>()
     .iter()
     .map(|&x| {*x})
@@ -38,28 +45,28 @@ fn l2d_not_self(a: Vec<f64>, b: Vec<f64>) -> f64 {
 
 fn l2d(a: Vec<f64>, b: Vec<f64>) -> f64 {
   let z = a
-  .iter()
-  .zip(b.iter());
+    .iter()
+    .zip(b.iter());
   let dist_sum: f64 = z
-  .into_iter()
-  .collect::<Vec<(&f64,&f64)>>()
-  .iter()
-  .fold(0_f64, |a, x| {a + (((*x).0 - (*x).1).powf(2_f64))});
+    .into_iter()
+    .collect::<Vec<(&f64,&f64)>>()
+    .iter()
+    .fold(0_f64, |a, x| {a + (((*x).0 - (*x).1).powf(2_f64))});
   dist_sum.sqrt()
 }
 
 fn f64max(l: Vec<f64>) -> f64 {
   l
-  .iter()
-  .cloned()
-  .fold(0./0., f64::max)
+    .iter()
+    .cloned()
+    .fold(0./0., f64::max)
 }
 
 fn f64min(l: Vec<f64>) -> f64 {
   l
-  .iter()
-  .cloned()
-  .fold(0./0., f64::min)
+    .iter()
+    .cloned()
+    .fold(0./0., f64::min)
 }
 
 fn median(l: Vec<f64>) -> f64 {
