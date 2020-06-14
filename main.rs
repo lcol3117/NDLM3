864fe26b7f893e7260b849e66e40ef7d32b2fc5b
Landon Colaresi <landoncol3117@gmail.com>
2020-06-14 17:47:03 +0000

    Finish crossing fn

--- main.rs
+++ main.rs
@@ -20,11 +20,11 @@ fn get_nd(point: Vec<f64>, data: Vec<Vec<f64>>) -> f64 {
 fn crossing(a: Vec<f64>, b: Vec<f64>, data: Vec<Vec<f64>>, ndt: Vec<f64>, eta: u32) -> bool {
   let a_index = data
     .iter()
-    .position(|&x| {x === a})
+    .position(|&x| {x.clone() === a.clone()})
     .unwrap();
   let b_index = data
     .iter()
-    .position(|&x| {x === b})
+    .position(|&x| {x.clone() === b.clone()})
     .unwrap();
   let a_index_usize = a_index as usize;
   let b_index_usize = b_index as usize;
@@ -37,6 +37,13 @@ fn crossing(a: Vec<f64>, b: Vec<f64>, data: Vec<Vec<f64>>, ndt: Vec<f64>, eta: u
     .collect::<Vec<f64>>();
   let outer_ndts = vec![a_ndt, b_ndt];
   let outer_ndts_min = f64min(outer_ndts);
+  let middle_ndts_accept = middle_ndts
+    .iter()
+    .filter(|&x| |(*x) < outer_ndts_min|)
+    .map(|x| {*x})
+    .collect::<Vec<f64>>();
+  let delta_ndt = middle_ndts_accept.len() / middle_ndts.len()
+  delta_ndt >= (1_f64 / 3_f64)
 }
 
 fn along_line(a: Vec<f64>, b: Vec<f64>, how_far: f64): Vec<f64> {
